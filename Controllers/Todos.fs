34b706158f5cb2f5b87e055ca6dba713d9dc84ee
maxmellon <tsujiken_melon@r.recruit.co.jp>
2019-08-16 16:15:57 +0900

    :memo:

--- Controllers/Todos.fs
+++ Controllers/Todos.fs
@@ -8,5 +8,7 @@ module TodosController =
     choose [
       GET >=> route "/todos" >=>
         fun next context ->
+          // Giraffe.ResponseWriters.text 
+          // see: https://github.com/giraffe-fsharp/Giraffe/blob/2e5dcae47b71c5fa12bbf84166681a11c910d951/src/Giraffe/ResponseWriters.fs#L301-L305
           text "Read" next context
     ]
