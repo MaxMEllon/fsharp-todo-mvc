45f3bf70ad637e56a19e76be8189a6ca08c737e9
maxmellon <tsujiken_melon@r.recruit.co.jp>
2019-08-16 16:08:42 +0900

    feat: imple Todo model and collection

--- Models/Todo.fs
+++ Models/Todo.fs
@@ -1,8 +1,19 @@
 namespace Models.Todo
 
-type Todo =
-  {
-    Id: string
-    Text: string
-    Done: bool
-  }
+type TodoModel(id: string, txt: string, status: bool) =
+  member this.Id = id
+  member this.Text = txt
+  member this.Done = status
+
+  new ((id: string, txt: string, status: bool)) = TodoModel(id, txt, status)
+
+  member this.Did() =
+    this.Done = true
+  
+  member this.UpdateText(txt: string) =
+    this.Text = txt
+
+
+type TodoCollection(arr: (string * string * bool) list) =
+  member this.Todos = List.map TodoModel arr
+    
